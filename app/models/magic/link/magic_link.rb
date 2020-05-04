4cb368766d731e926a9301dd5a09bcee08e56350
Joel Niedfeldt <joel@niedfeldt.com>
2020-05-04 14:53:51 -0300

    remove path/redirect code

--- app/models/magic/link/magic_link.rb
+++ app/models/magic/link/magic_link.rb
@@ -1,19 +1,17 @@
 module Magic
   module Link
     class MagicLink
-      attr_accessor :email, :user, :path, :redirect_id, :reusable
+      attr_accessor :email, :user, :reusable
 
-      def initialize(email:, path: nil, redirect_id: nil, reusable: false)
+      def initialize(email:, reusable: false)
         @email = email
-        @path = path 
-        @redirect_id = redirect_id
         @reusable = reusable
         @user = Magic::Link.user_class.find_by(email: email.downcase)
       end 
 
       def send_login_instructions
         token = get_login_token
-        MagicLinkMailer.send_magic_link(email, token, path, redirect_id)
+        MagicLinkMailer.send_magic_link(email, token)
       end
 
       def get_login_token
--- lib/magic/link/controller_extensions.rb
+++ lib/magic/link/controller_extensions.rb
@@ -22,37 +22,15 @@ module Magic
             if !magic_link_token_expired?(token)
               token.destroy unless token.reusable?
               sign_in resource
-              check_magic_link_redirect!
             else 
               flash[:alert] = "That link was expired, but we just sent you a new one. Please click that link to login."
-              path_param = params[:path].presence
-              redirect_id_param = params[:redirect_id].presence 
-              new_magic_link = MagicLink.new(email: resource.email, path: path_param, redirect_id: redirect_id_param, reusable: token.reusable?)
+              new_magic_link = MagicLink.new(email: resource.email, reusable: token.reusable?)
               new_magic_link.send_login_instructions.deliver_now
               token.destroy
             end   
           else
             flash[:alert] = "Token or email invalid. Please sign in to access that page."
             redirect_to main_app.new_user_session_path
-          end
-        end
-
-        def check_magic_link_redirect!
-          path_param = params[:path].presence
-          redirect_id_param = params[:redirect_id].presence
-
-          return unless path_param
-
-          redirect_to_magic_link_path(path_param, redirect_id_param)
-        end 
-
-        def redirect_to_magic_link_path(path, redirect_id)
-          if path && redirect_id
-            redirect_to send("#{path}_path".to_sym, redirect_id)
-          elsif path
-            redirect_to "#{path}".to_sym
-          else
-            redirect_to admin_dashboard_url
           end
         end
 
