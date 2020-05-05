7d9ac5ad94ed2023c3ed9e37c8b5a00c216221a6
Joel Niedfeldt <joel@niedfeldt.com>
2020-05-05 13:25:13 -0300

    add helper to mailer and fix logic for already signed in

--- lib/magic/link/controller_extensions.rb
+++ lib/magic/link/controller_extensions.rb
@@ -17,7 +17,7 @@ module Magic
           token = Token.find_by(token: Devise.token_generator.digest(Token, :token, token_param))
 
           if send("#{Magic::Link.user_class.name.underscore}_signed_in?")
-            check_magic_link_redirect!
+            token.destroy unless token.nil? || token.reusable?
           elsif magic_link_token_matches?(resource, token)
             if !magic_link_token_expired?(token)
               token.destroy unless token.reusable?
