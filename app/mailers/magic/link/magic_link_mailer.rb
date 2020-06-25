1e521ab7671213348c4c7432eae1efdd28196f0e
Joel Niedfeldt <joel@niedfeldt.com>
2020-06-25 16:13:53 -0300

    fixing magic link to do redirect

--- app/mailers/magic/link/magic_link_mailer.rb
+++ app/mailers/magic/link/magic_link_mailer.rb
@@ -1,10 +1,13 @@
 module Magic::Link
   class MagicLinkMailer < ApplicationMailer
-    def send_magic_link(email, token, url: nil, params: [])
+    def send_magic_link(email, token, url: nil)
       @email = email
       @token = token
-      @url = url 
-      @params = params
+      if url 
+        uri = URI url
+        uri.query = CGI.parse(uri.query).merge({"email" => @email, "sign_in_token" => @token}).to_query
+        @url = uri.to_s 
+      end
       mail(to: email, subject: "Your sign in link")
     end
   end
